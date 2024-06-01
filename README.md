8300235107a23faa6c1483b84e096257e7b3d6e1
shantanu dhar Das <146721097+shantanu4036@users.noreply.github.com>
2024-06-01 20:35:52 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+deploy link:https://main--effulgent-rabanadas-a3741f.netlify.app/
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    screens: {
+      '2xl': {'max': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': {'max': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': {'max': '1030px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'md': {'max': '700px'},
+      // => @media (max-width: 767px) { ... }
+
+      'sm': {'max': '639px'},
+      // => @media (max-width: 639px) { ... }
+    }
+  },
+  plugins: [],
+}
